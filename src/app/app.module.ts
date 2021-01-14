d865811a046f51c8a2cda8f357f91f4b1f525d48
BITCUBICO Technology <bitcubico@gmail.com>
2021-01-14 12:19:38 -0500

    Agrego los componentes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreciosComponent,
+    ProtegidaComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
